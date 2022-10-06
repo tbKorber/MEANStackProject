05e0aa16b21d5f743635096c0842006230bf5e02
Trevor Körber <97142040+tbKorber@users.noreply.github.com>
2022-10-06 17:34:18 +0800

    init

---
+++ src/app/posts/post-create/post-create.component.ts
@@ -0,0 +1,19 @@
+import { Component} from "@angular/core";
+import { NgForm } from '@angular/forms'
+import { PostsService } from "../post.service";
+
+@Component({
+  selector: 'app-post-create',
+  templateUrl: './post-create.component.html',
+  styleUrls: ['./post-create.component.less']
+})
+export class PostCreateComponent {
+
+  constructor(public postsService: PostsService) {}
+
+  onAddPost(form: NgForm) {
+    if(form.invalid){return}
+
+    this.postsService.addPost(form.value.title, form.value.content)
+  }
+}
